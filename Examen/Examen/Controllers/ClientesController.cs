99b1635b750c0db6be5e6ba5c6c5233fdb53c718
Doncepillo <daniel.perezz@correoaiep.cl>
2017-12-29 20:45:54 -0300

    Examen Taller Aplicaciones para Internet en Net

---
+++ Examen/Examen/Controllers/ClientesController.cs
@@ -0,0 +1,78 @@
+﻿using Examen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Examen.Controllers
+{
+    public class ClientesController : Controller
+    {
+        ExamenEntities cnx;
+
+        public ClientesController()
+        {
+            cnx = new ExamenEntities();
+
+        }
+        public ActionResult NuevoCliente()
+        {
+
+            return View();
+        }
+
+
+        public ActionResult Listado()
+        {
+
+            cnx.Database.Connection.Open();
+
+            List<Cliente> cliente = cnx.Cliente.ToList();
+            cnx.Database.Connection.Close();
+            return View(cliente);
+        }
+
+
+
+        public ActionResult Guardar(string Rut, string Nombre, string Apellidos, string Direccion, string TipoCliente)
+        {
+            Cliente cliente = new Cliente()
+            {
+                Rut = Rut,
+                Nombre = Nombre,
+                Apellidos = Apellidos,
+                Direccion = Direccion,
+                TipoCliente = TipoCliente
+
+            };
+
+            cnx.Cliente.Add(cliente);
+            cnx.SaveChanges();
+            return View("Listado", cnx.Cliente.ToList());
+
+        }
+
+
+
+        public ActionResult Ficha(int Id)
+        {
+
+            return View(cnx.Cliente.Where(x => x.Id == Id).First());
+
+        }
+
+
+
+
+        public ActionResult Ver(int Id)
+        {
+
+
+            return View("Ficha", null);
+        }
+
+
+
+    }
+}
---
+++ Examen/Examen/Controllers/ProductosController.cs
@@ -0,0 +1,80 @@
+﻿using Examen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Examen.Controllers
+{
+    public class ProductosController : Controller
+    {
+            ExamenEntities cnx;
+
+            public ProductosController()
+            {
+                cnx = new ExamenEntities();
+
+            }
+            public ActionResult NuevoProducto()
+            {
+
+                return View();
+            }
+
+
+            public ActionResult Listado()
+            {
+
+                cnx.Database.Connection.Open();
+
+                List<Producto> producto = cnx.Producto.ToList();
+                cnx.Database.Connection.Close();
+                return View(producto);
+            }
+
+
+
+            public ActionResult Guardar(string Nombre, string CodigoBarra, string Familia, int Precio, int Descuento, string Descripcion)
+            {
+                Producto producto = new Producto()
+                {
+
+                    Nombre = Nombre,
+                    CodigoBarra = CodigoBarra,
+                    Familia = Familia,
+                    Precio = Precio,
+                    Descuento = Descuento,
+                    Descripcion = Descripcion
+
+                };
+
+                cnx.Producto.Add(producto);
+                cnx.SaveChanges();
+                return View("Listado", cnx.Producto.ToList());
+
+            }
+
+
+
+            public ActionResult Ficha(int Id)
+            {
+
+                return View(cnx.Producto.Where(x => x.Id == Id).First());
+
+            }
+
+
+
+
+            public ActionResult Ver(int Id)
+            {
+
+
+                return View("Ficha", null);
+            }
+
+
+          
+    }
+}
